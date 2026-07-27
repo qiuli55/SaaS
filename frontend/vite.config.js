990dfdbdc76804e0e60982c8847f342f41a1f1fb
qiuli55 <qiuli55@users.noreply.github.com>
2026-07-28 04:21:44 +0800

    Vite配置外部访问: host=0.0.0.0, hmr指定内网IP

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,11 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   server: {
+    host: '0.0.0.0',
     port: 5173,
+    hmr: {
+      host: '10.137.81.78',
+    },
     proxy: {
       '/api': {
         target: 'http://127.0.0.1:8000',
